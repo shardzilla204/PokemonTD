4ceeeb5012cd8c1787a132cb6542ac960ba1c0e1
shardzilla204 <shardzilla204@gmail.com>
2025-06-10 20:53:12 -0400

    Fix Bugs

--- Resources/Scripts/PokemonSettings.cs
+++ Resources/Scripts/PokemonSettings.cs
@@ -19,11 +19,18 @@ public partial class PokemonSettings : Node
     private string _settingsFilePath = "user://settings.cfg";
 
     public bool HasShowedTutorial = false;
-    public int WindowModeIndex = 0;
+    public int WindowModeIndex = 3;
+    public Vector2I WindowSize = DisplayServer.WindowGetSize();
+    public bool IsWindowMaximized = false;
     public bool ButtonSFXEnabled = true;
     public bool PokemonSFXEnabled = true;
     public bool PokemonMoveSFXEnabled = true;
     public bool AutoHealEnabled = false;
+    
+    public override void _ExitTree()
+    {
+        SaveSettings();
+    }
 
     public override void _EnterTree()
     {
@@ -31,6 +38,15 @@ public partial class PokemonSettings : Node
 
         PokemonTD.Signals.GameSaved += SaveSettings;
         PokemonTD.Signals.GameLoaded += LoadSettings;
+        GetWindow().SizeChanged += () =>
+        {
+            WindowSize = GetWindow().Size;
+        };
+
+        GetWindow().WindowInput += (inputEvent) =>
+        {
+            IsWindowMaximized = DisplayServer.WindowGetMode() == DisplayServer.WindowMode.Maximized;
+        };
 
         if (FileAccess.FileExists(_settingsFilePath))
         {
@@ -103,12 +119,24 @@ public partial class PokemonSettings : Node
             { "Audio", GetAudioData() },
             { "Has Showed Tutorial", HasShowedTutorial },
             { "Window Mode Index", WindowModeIndex },
+            { "Window Size", GetWindowSizeData() },
+            { "Is Window Maximized", IsWindowMaximized },
             { "Button SFX Enabled", ButtonSFXEnabled },
             { "Pokemon SFX Enabled", PokemonSFXEnabled },
             { "Pokemon Move SFX Enabled", PokemonMoveSFXEnabled },
             { "Auto Heal Enabled", AutoHealEnabled }
         };
         return settingsData;
+    }
+
+    private GC.Dictionary<string, Variant> GetWindowSizeData()
+    {
+        GC.Dictionary<string, Variant> windowSizeData = new GC.Dictionary<string, Variant>()
+        {
+            { "X", WindowSize.X },
+            { "Y", WindowSize.Y }
+        };
+        return windowSizeData;
     }
 
     private GC.Dictionary<string, Variant> GetAudioData()
@@ -137,6 +165,9 @@ public partial class PokemonSettings : Node
 
         HasShowedTutorial = settingsData["Has Showed Tutorial"].As<bool>();
         WindowModeIndex = settingsData["Window Mode Index"].As<int>();
+        GC.Dictionary<string, Variant> windowSizeData = settingsData["Window Size"].As<GC.Dictionary<string, Variant>>();
+        SetWindowSizeData(windowSizeData);
+        IsWindowMaximized = settingsData["Is Window Maximized"].As<bool>();
         ButtonSFXEnabled = settingsData["Button SFX Enabled"].As<bool>();
         PokemonSFXEnabled = settingsData["Pokemon SFX Enabled"].As<bool>();
         PokemonMoveSFXEnabled = settingsData["Pokemon Move SFX Enabled"].As<bool>();
@@ -145,13 +176,20 @@ public partial class PokemonSettings : Node
         ApplySettings();
     }
 
+    private void SetWindowSizeData(GC.Dictionary<string, Variant> windowSizeData)
+    {
+        int x = windowSizeData["X"].As<int>();
+        int y = windowSizeData["Y"].As<int>();
+        WindowSize = new Vector2I(x, y);
+    }
+
     private void SetAudioData(GC.Dictionary<string, Variant> audioData)
     {
         foreach (AudioBus audioBus in PokemonTD.AudioManager.AudioBuses)
         {
             GC.Dictionary<string, Variant> busData = audioData[audioBus.BusType.ToString()].As<GC.Dictionary<string, Variant>>();
             SetBusData(audioBus, busData);
-            AudioServer.SetBusVolumeDb((int) audioBus.BusType, audioBus.Volume);
+            AudioServer.SetBusVolumeDb((int)audioBus.BusType, audioBus.Volume);
         }
     }
 
@@ -163,6 +201,8 @@ public partial class PokemonSettings : Node
 
     public void ApplySettings()
     {
+        Vector2I windowSize = WindowSize;
+
         DisplayServer.MouseMode mouseMode = WindowModeIndex == 0 ? DisplayServer.MouseMode.Confined : DisplayServer.MouseMode.Visible;
         DisplayServer.MouseSetMode(mouseMode);
 
@@ -171,6 +211,13 @@ public partial class PokemonSettings : Node
             2 => DisplayServer.WindowMode.Windowed,
             _ => DisplayServer.WindowMode.Fullscreen
         };
+
+        if (IsWindowMaximized && windowMode == DisplayServer.WindowMode.Windowed)
+        {
+            windowMode = DisplayServer.WindowMode.Maximized;
+        }
+
         DisplayServer.WindowSetMode(windowMode);
+        GetWindow().Size = windowSize;
     }
 }
